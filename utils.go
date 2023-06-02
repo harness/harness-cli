2462d236620c2877fc5dc85d69af779ac12b6d43
Devinder Prasad <devinder.prasad@harness.io>
2023-06-01 17:10:35 -0700

    fix: added utils to hydrate sessiom state from persistence
    fix: removed temp and binaries

--- utils.go
+++ utils.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"github.com/AlecAivazis/survey/v2"
 	log "github.com/sirupsen/logrus"
+	"io"
 	"os"
+	"strings"
 )
 
 func ConfirmInput(question string) bool {
@@ -47,4 +49,70 @@ func printJson(v any) {
 		log.Fatalf("Marshaling error: %s", err)
 	}
 	fmt.Println("Data: ", string(marsheld))
+}
+
+func writeToFile(text string, filepath string, overwrite bool) {
+	var permissions = os.O_APPEND | os.O_CREATE | os.O_WRONLY
+	if overwrite {
+		permissions = os.O_APPEND | os.O_CREATE | os.O_WRONLY | os.O_TRUNC
+	}
+	f, err := os.OpenFile(filepath, permissions, 0644)
+	if overwrite {
+		f.WriteString("")
+	}
+	f.WriteString(text)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.Close()
+}
+
+func readFromFile(filepath string) (s string) {
+	var _fileContents = ""
+	buffer := make([]byte, 1024)
+	file, fileError := os.OpenFile(filepath, os.O_RDONLY, 0644)
+	defer file.Close()
+	for {
+		reader, readError := file.Read(buffer)
+		if readError != nil {
+			if readError == io.EOF {
+				break
+			} else {
+				log.Println("Error reading from file:", fileError)
+				break
+			}
+		}
+		_fileContents = string(buffer[:reader])
+	}
+	return _fileContents
+}
+
+func saveCredentials() (err error) {
+	credString := ""
+	credString += "token:" + cliCdRequestData.AuthToken
+	credString += "\n"
+	credString += "accountId:" + cliCdRequestData.Account
+	writeToFile(credString, TEMP_FILE_NAME, true)
+	return nil
+}
+func hydrateCredsFromPersistence() (token string, id string) {
+	if cliCdRequestData.AuthToken != "" && cliCdRequestData.Account != "" {
+		return
+	}
+	credsText := readFromFile(TEMP_FILE_NAME)
+	credentialsArray := strings.Split(credsText, "\n")
+	cliCdRequestData.AuthToken = getParsedAuthKey(credentialsArray[0])
+	cliCdRequestData.Account = getParsedAccountId(credentialsArray[1])
+	return cliCdRequestData.AuthToken, cliCdRequestData.Account
+}
+
+func getParsedAuthKey(credsText string) (token string) {
+	authKey := strings.Split(credsText, ":")[1]
+	return authKey
+}
+
+func getParsedAccountId(credsText string) (token string) {
+	accid := strings.Split(credsText, ":")[1]
+	return accid
 }
