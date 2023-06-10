3f0d7fa8620ddb4155b9f3e687def496d7b0716e
nekum1 <117125370+neelam-harness@users.noreply.github.com>
2023-06-09 18:14:35 -0700

    feat: [PLG-2449] Added logic for pipeline apply and some refactoring

--- constants.go
+++ constants.go
@@ -1,11 +1,17 @@
 package main
 
-const NG_BASE_URL = "https://app.harness.io/gateway/ng"
+const CONTENT_TYPE_JSON = "application/json"
+const CONTENT_TYPE_YAML = "application/yaml"
 const ENVIRONMENT_ENDPOINT = "environmentsV2"
 const GITHUB_SECRET_IDENTIFIER = "harness_gitpat"
+<<<<<<< HEAD
 const DEFAULT_PROJECT = "default_project"
 const DEFAULT_ORG = "default"
 const JSON_CONTENT_TYPE = "application/json"
+=======
+const NG_BASE_URL = "https://app.harness.io/gateway/ng/api"
+>>>>>>> 3bc0f18 (feat: [PLG-2449] Added logic for pipeline apply and some refactoring)
 const NOT_IMPLEMENTED = "Command Not_Implemented. Check back later.."
-const PIPELINES_ENDPOINT = "pipelines"
+const PIPELINES_BASE_URL = "https://app.harness.io/pipeline/api"
+const PIPELINES_ENDPOINT = "pipelines/v2"
 const SECRETS_STORE_PATH = ".secrets.json"
