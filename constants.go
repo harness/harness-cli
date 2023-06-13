97f92d27c229eb0dae00ad8118f7822f9ec9a6c9
Devinder Prasad <devinder.prasad@harness.io>
2023-06-12 19:51:26 -0700

    fix: [PLG-2487]: flags token

--- constants.go
+++ constants.go
@@ -3,6 +3,8 @@ package main
 const BASE_URL = "https://app.harness.io/gateway/ng"
 const ENVIRONMENT_ENDPOINT = "environmentsV2"
 const GITHUB_SECRET_IDENTIFIER = "harness_gitpat"
+const DEFAULT_PROJECT = "default_project"
+const DEFAULT_ORG = "default"
 const JSON_CONTENT_TYPE = "application/json"
 const NOT_IMPLEMENTED = "Command Not_Implemented. Check back later.."
 const SECRETS_STORE_PATH = ".secrets.json"
