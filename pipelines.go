e5fa87f29120f2b927291a3707f61028ed7187bd
nekum1 <117125370+neelam-harness@users.noreply.github.com>
2023-06-14 16:23:58 -0700

    fix: working pipelines logic for POST and PUT

--- pipelines.go
+++ pipelines.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
 )
@@ -12,37 +13,28 @@ func applyPipeline(c *cli.Context) error {
 	fmt.Println("Trying to create or update pipeline using the given yaml.")
 
 	var content = readFromFile(c.String("file"))
-
 	requestBody := getJsonFromYaml(content)
 	if requestBody == nil {
 		println(getColoredText("Please enter valid pipeline yaml file", color.FgRed))
 	}
+
 	identifier := valueToString(GetNestedValue(requestBody, "pipeline", "identifier").(string))
-	name := valueToString(GetNestedValue(requestBody, "pipeline", "name").(string))
 	projectIdentifier := valueToString(GetNestedValue(requestBody, "pipeline", "projectIdentifier").(string))
 	orgIdentifier := valueToString(GetNestedValue(requestBody, "pipeline", "orgIdentifier").(string))
-	//branch := valueToString(GetNestedValue(requestBody, "pipeline", "branch").(string))
-	//fmt.Printf("identifier=%s, name=%s", identifier, name)
 	createOrUpdatePipelineURL := GetUrlWithQueryParams("", PIPELINES_BASE_URL, PIPELINES_ENDPOINT_V2, map[string]string{
 		"accountIdentifier": cliCdRequestData.Account,
 		"orgIdentifier":     orgIdentifier,
 		"projectIdentifier": projectIdentifier,
 	})
-	//setup payload for Pipelines create / update
-	fmt.Println("PipelinePayload: ")
-	pipelinePayload := HarnessPipeline{Identifier: identifier, Name: name,
-		ProjectIdentifier: projectIdentifier, OrgIdentifier: orgIdentifier, Yaml: content}
-	printJson(pipelinePayload)
-
-	entityExists := getEntity(PIPELINES_BASE_URL, fmt.Sprintf("%s/%s", PIPELINES_ENDPOINT_V2, identifier),
+	entityExists := getEntity(PIPELINES_BASE_URL, fmt.Sprintf("%s/%s", PIPELINES_ENDPOINT, identifier),
 		projectIdentifier, orgIdentifier, map[string]string{})
 	var resp ResponseBody
 	var err error
 	if !entityExists {
 		println("Creating pipeline with id: ", getColoredText(identifier, color.FgGreen))
-		//fmt.Println("createOrUpdatePipelineURL: ", createOrUpdatePipelineURL)
-		//fmt.Println("requestBody: ", requestBody)
-		resp, err = Post(createOrUpdatePipelineURL, cliCdRequestData.AuthToken, content, CONTENT_TYPE_YAML)
+		fmt.Println("createOrUpdatePipelineURL: ", createOrUpdatePipelineURL)
+		fmt.Println("requestBody: ", requestBody)
+		resp, err = Post(createOrUpdatePipelineURL, cliCdRequestData.AuthToken, requestBody, CONTENT_TYPE_YAML)
 
 		if err == nil {
 			println(getColoredText("Pipeline created successfully!", color.FgGreen))
@@ -50,12 +42,18 @@ func applyPipeline(c *cli.Context) error {
 			return nil
 		}
 	} else {
+		var pipelinesPUTUrl = GetUrlWithQueryParams("", PIPELINES_BASE_URL,
+			fmt.Sprintf("%s/%s", PIPELINES_ENDPOINT_V2, identifier), map[string]string{
+				"pipelineIdentifier": identifier,
+				"accountIdentifier":  cliCdRequestData.Account,
+				"orgIdentifier":      orgIdentifier,
+				"projectIdentifier":  projectIdentifier,
+			})
 		println("Found Pipeline with id: ", getColoredText(identifier, color.FgGreen))
 		println(getColoredText("Updating existing Pipeline details....", color.FgGreen))
-		resp, err = Put(createOrUpdatePipelineURL, cliCdRequestData.AuthToken, pipelinePayload, CONTENT_TYPE_YAML)
+		resp, err = Put(pipelinesPUTUrl, cliCdRequestData.AuthToken, requestBody, CONTENT_TYPE_YAML)
 		if err == nil {
 			println(getColoredText("Pipeline updated successfully!", color.FgGreen))
-			//printJson(resp.Data)
 			return nil
 		}
 	}
