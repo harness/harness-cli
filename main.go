e33050ccefe00690d2e30610be53ec135b9469c2
Debanitrkl <debabrata.panigrahi@harness.io>
2023-08-31 08:54:01 +0530

    PLG-2770 Changed the account-id description

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
 			Name:        "account-id",
-			Usage:       "`API_KEY` for the target account to authenticate & authorise the user.",
+			Usage:       "provide the auto-generated identifier for target account",
 			Destination: &cliCdRequestData.Account,
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
