3a31c7bf4e2b3ffe94fd992219aba1a36478228a
Debabrata Panigrahi <50622005+Debanitrkl@users.noreply.github.com>
2023-09-15 19:44:46 +0530

    Update gitops-application.go
    Co-authored-by: OP (oppenheimer) <21008429+Ompragash@users.noreply.github.com>

--- gitops-application.go
+++ gitops-application.go
@@ -67,7 +67,7 @@ func applyGitopsApplications(c *cli.Context) error {
 		}
 
 	} else {
-		println("Found GitOps-Application with id=", getColoredText(applicationName, color.FgCyan))
+		println("Found GitOps Application with id=", getColoredText(applicationName, color.FgCyan))
 		println("Updating details of GitOps-Application with id=", getColoredText(applicationName, color.FgBlue))
 
 		var appPUTUrl = GetUrlWithQueryParams("", baseURL,
