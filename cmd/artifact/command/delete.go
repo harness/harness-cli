45789ec4eff06295ddfccc2eab79d3738df9a596
Ashwini Chourasia <c_ashwini.chourasia@harness.io>
2026-04-10 19:20:44 +0530

    concurrent delete added

--- cmd/artifact/command/delete.go
+++ cmd/artifact/command/delete.go
@@ -2,8 +2,8 @@ package command
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"sync"
 
 	"github.com/harness/harness-cli/cmd/artifact/command/utils"
 	"github.com/harness/harness-cli/cmd/cmdutils"
@@ -25,7 +25,7 @@ func NewDeleteArtifactCmd(c *cmdutils.Factory) *cobra.Command {
 
 			// If config file provided from flag is provided, only then execute bulk delete
 			if configPath != "" {
-				executeBulkDelete(configPath)
+				executeBulkDelete(c, configPath)
 				return nil
 			}
 			registryRef := client2.GetRef(client2.GetScopeRef(), registry)
@@ -58,6 +58,8 @@ func NewDeleteArtifactCmd(c *cmdutils.Factory) *cobra.Command {
 }
 
 func performVersionDelete(c *cmdutils.Factory, registryRef, name, version string) error {
+
+	fmt.Println(registryRef)
 	response, err := c.RegistryHttpClient().DeleteArtifactVersionWithResponse(context.Background(), registryRef, name, version)
 	if err != nil {
 		return err
@@ -83,7 +85,7 @@ func performArtifactDelete(c *cmdutils.Factory, registryRef, name string) error 
 	return nil
 }
 
-func executeBulkDelete(configPath string) {
+func executeBulkDelete(c *cmdutils.Factory, configPath string) {
 	if configPath == "" {
 		return
 	}
@@ -94,28 +96,110 @@ func executeBulkDelete(configPath string) {
 		return
 	}
 
-	configJSON, err := json.MarshalIndent(config, "", "  ")
+	/*configJSON, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		log.Error().Msgf("Failed to marshal config to JSON: %v", err)
 		return
 	}
 
 	log.Info().Msgf("Bulk delete configuration loaded from %s", configPath)
-	fmt.Println("=== Bulk Delete Configuration ===")
 	log.Info().Msgf(string(configJSON))
 	log.Info().Msgf("\n=== Detailed Registry Information ===")
 
+	*/
+
+	// Collect all deletion jobs
+	var jobs []deleteJob
 	for registryName, packages := range config.Registries {
-		log.Info().Msgf("\nRegistry: %s\n", registryName)
+		//log.Info().Msgf("\nRegistry: %s\n", registryName)
+		registryRef := client2.GetRef(client2.GetScopeRef(), registryName)
+
 		for packageName, versions := range packages {
-			log.Info().Msgf("  Package: %s\n", packageName)
+			//log.Info().Msgf("  Package: %s\n", packageName)
 			if len(versions) == 0 {
-				log.Info().Msgf("    Versions: [] (delete all versions)\n")
-				//call version delete API
+				//log.Info().Msgf("    Versions: [] (delete all versions)\n")
+				// creating complete Artifact delete job
+				jobs = append(jobs, deleteJob{
+					jobType:      "artifact",
+					registryName: registryName,
+					registryRef:  registryRef,
+					packageName:  packageName,
+				})
 			} else {
-				fmt.Printf("    Versions: %v\n", versions)
-				// call package delete API , API Is same but  parameter is different
+				log.Info().Msgf("    Versions: %v\n", versions)
+				// creating version delete job
+				for _, version := range versions {
+					jobs = append(jobs, deleteJob{
+						jobType:      "version",
+						registryName: registryName,
+						registryRef:  registryRef,
+						packageName:  packageName,
+						version:      version,
+					})
+				}
 			}
 		}
 	}
+
+	log.Info().Msgf("\nTotal deletion jobs collected: %d", len(jobs))
+	log.Info().Msgf("Starting concurrent deletion with concurrency: %d\n", config.Concurrency)
+
+	// Execute deletions concurrently
+	processDeletionsConcurrently(c, jobs, config.Concurrency)
+}
+
+func processDeletionsConcurrently(c *cmdutils.Factory, jobs []deleteJob, concurrency int) {
+	jobChan := make(chan deleteJob, len(jobs))
+	var wg sync.WaitGroup
+
+	// Start worker goroutines
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			for job := range jobChan {
+				if job.jobType == "version" {
+					log.Info().Msgf("[Worker %d] Deleting version %s : %s from registry %s",
+						workerID, job.packageName, job.version, job.registryName)
+					err := performVersionDelete(c, job.registryRef, job.packageName, job.version)
+					if err != nil {
+						log.Error().Msgf("[Worker %d] Failed to delete version %s : %s - %v",
+							workerID, job.packageName, job.version, err)
+					} else {
+						log.Info().Msgf("[Worker %d] Successfully deleted version %s :%s",
+							workerID, job.packageName, job.version)
+					}
+				} else if job.jobType == "artifact" {
+					log.Info().Msgf("[Worker %d] Deleting artifact %s (all versions) from registry %s",
+						workerID, job.packageName, job.registryName)
+					err := performArtifactDelete(c, job.registryRef, job.packageName)
+					if err != nil {
+						log.Error().Msgf("[Worker %d] Failed to delete artifact %s - %v",
+							workerID, job.packageName, err)
+					} else {
+						log.Info().Msgf("[Worker %d] Successfully deleted artifact %s",
+							workerID, job.packageName)
+					}
+				}
+			}
+		}(i + 1)
+	}
+
+	// Send jobs to channel
+	for _, job := range jobs {
+		jobChan <- job
+	}
+	close(jobChan)
+
+	// Wait for all workers to complete
+	wg.Wait()
+	log.Info().Msgf("\nBulk deletion completed. Processed %d jobs.", len(jobs))
+}
+
+type deleteJob struct {
+	jobType      string // "version" or "artifact"
+	registryName string
+	registryRef  string
+	packageName  string
+	version      string // empty for artifact deletion
 }
