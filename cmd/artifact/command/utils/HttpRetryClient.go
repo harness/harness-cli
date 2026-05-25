08f15699f4883858ff243d2cde3f4fcfd6762b17
Ashwini Chourasia <c_ashwini.chourasia@harness.io>
2026-05-25 19:45:53 +0530

    added custom retry client

--- cmd/artifact/command/utils/HttpRetryClient.go
+++ cmd/artifact/command/utils/HttpRetryClient.go
@@ -4,34 +4,49 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/harness/harness-cli/internal/api/ar_pkg"
-	"github.com/harness/harness-cli/internal/api/ar_v3"
 	p "github.com/harness/harness-cli/util/common/progress"
 
 	retryablehttp "github.com/hashicorp/go-retryablehttp"
 )
 
-func GetRetryClient(progress p.Reporter) ar_pkg.ClientOption {
-	return ar_pkg.WithHTTPClient(NewRetryClient(progress))
-}
-
-// GetRetryClientV3 creates a retry client option for ar_v3 API client without progress reporting.
-// Use this when you don't have access to a progress reporter (e.g., in factory methods).
-func GetRetryClientV3() ar_v3.ClientOption {
-	return ar_v3.WithHTTPClient(NewRetryClientWithoutProgress())
-}
-
-// GetRetryClientV3WithProgress creates a retry client option for ar_v3 API client with progress reporting.
-func GetRetryClientV3WithProgress(progress p.Reporter) ar_v3.ClientOption {
-	return ar_v3.WithHTTPClient(NewRetryClient(progress))
-}
-
-// NewRetryClientWithoutProgress creates a retryable HTTP client without progress reporting hooks.
+// creates a retryable HTTP client without progress reporting hooks.
 // Use this when you don't have access to a progress reporter.
 func NewRetryClientWithoutProgress() *http.Client {
 	retryClient := retryablehttp.NewClient()
 	retryClient.RetryMax = 3
 	retryClient.Logger = nil
+	progress := p.NewConsoleReporter()
+	retryClient.RequestLogHook = func(
+		_ retryablehttp.Logger,
+		req *http.Request,
+		retryNumber int,
+	) {
+		if retryNumber > 0 {
+			//TODO format message
+			progress.Step(fmt.Sprintf(
+				"Retrying request: %s %s (attempt %d/%d)",
+				req.Method,
+				req.URL.String(),
+				retryNumber,
+				retryClient.RetryMax,
+			))
+		}
+	}
+
+	retryClient.ResponseLogHook = func(
+		_ retryablehttp.Logger,
+		resp *http.Response,
+	) {
+		if resp != nil {
+			//TODO switch statusCode and print message as per response
+			progress.Step(fmt.Sprintf(
+				"Request failed: %s %s -> status %d",
+				resp.Request.Method,
+				resp.Request.URL.String(),
+				resp.StatusCode,
+			))
+		}
+	}
 	return retryClient.StandardClient()
 }
 
--- cmd/cmdutils/factory.go
+++ cmd/cmdutils/factory.go
@@ -1,6 +1,7 @@
 package cmdutils
 
 import (
+	"github.com/harness/harness-cli/cmd/artifact/command/utils"
 	"github.com/harness/harness-cli/config"
 	"github.com/harness/harness-cli/internal/api/ar"
 	"github.com/harness/harness-cli/internal/api/ar_pkg"
@@ -21,7 +22,7 @@ type Factory struct {
 
 func (f *Factory) NewRegistryV3HttpClientWithURL(url string) (*ar_v3.ClientWithResponses, error) {
 	return ar_v3.NewClientWithResponses(url,
-		ar_v3.WithHTTPClient(auth.NewRetryableHTTPClient()),
+		ar_v3.WithHTTPClient(utils.NewRetryClientWithoutProgress()),
 		auth.GetXApiKeyOptionARV3())
 }
 
@@ -29,7 +30,7 @@ func NewFactory() *Factory {
 	return &Factory{
 		RegistryHttpClient: func() *ar.ClientWithResponses {
 			client, err := ar.NewClientWithResponses(config.Global.APIBaseURL+"/gateway/har/api/v1",
-				ar.WithHTTPClient(auth.NewRetryableHTTPClient()),
+				ar.WithHTTPClient(utils.NewRetryClientWithoutProgress()),
 				auth.GetXApiKeyOptionAR())
 			if err != nil {
 				log.Fatal().Msgf("Error creating client: %v", err)
@@ -38,7 +39,7 @@ func NewFactory() *Factory {
 		},
 		RegistryV2HttpClient: func() *ar_v2.ClientWithResponses {
 			client, err := ar_v2.NewClientWithResponses(config.Global.APIBaseURL+"/gateway/har/api/v2",
-				ar_v2.WithHTTPClient(auth.NewRetryableHTTPClient()),
+				ar_v2.WithHTTPClient(utils.NewRetryClientWithoutProgress()),
 				auth.GetXApiKeyOptionARV2())
 			if err != nil {
 				log.Fatal().Msgf("Error creating client: %v", err)
@@ -47,7 +48,7 @@ func NewFactory() *Factory {
 		},
 		RegistryV3HttpClient: func() *ar_v3.ClientWithResponses {
 			client, err := ar_v3.NewClientWithResponses(config.Global.APIBaseURL+"/gateway/har/api/v3",
-				ar_v3.WithHTTPClient(auth.NewRetryableHTTPClient()),
+				ar_v3.WithHTTPClient(utils.NewRetryClientWithoutProgress()),
 				auth.GetXApiKeyOptionARV3())
 			if err != nil {
 				log.Fatal().Msgf("Error creating client: %v", err)
@@ -56,7 +57,7 @@ func NewFactory() *Factory {
 		},
 		PkgHttpClient: func() *ar_pkg.ClientWithResponses {
 			client, err := ar_pkg.NewClientWithResponses(config.Global.Registry.PkgURL,
-				ar_pkg.WithHTTPClient(auth.NewRetryableHTTPClient()),
+				ar_pkg.WithHTTPClient(utils.NewRetryClientWithoutProgress()),
 				auth.GetAuthOptionARPKG())
 			if err != nil {
 				log.Fatal().Msgf("Error creating pkg client: %v", err)
